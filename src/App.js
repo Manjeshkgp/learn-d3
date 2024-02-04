e091a1d18b49c92ef4890b7244039b0a131131e6
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-02-05 00:07:54 +0530

    d3 art with rect.class selection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { select, range } from "d3";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { innerHeight: height, innerWidth: width } = window;
+
+  useEffect(() => {
+    const svg = select("#root").append("svg");
+    svg.attr("width", width).attr("height", height);
+
+    const n = 100;
+    
+    svg
+      .selectAll("rect.horizontal")
+      .data(range(n))
+      .join("rect")
+      .attr("y", (d) => d * 20)
+      .attr("width", width)
+      .attr("height", 10)
+      .attr("class", "horizontal")
+      .attr("mask", "url(#circle-mask)");
+
+    const circleMask = svg.append("mask").attr("id", "circle-mask");
+    const smallRadius = width > height ? height / 3 : width / 3;
+    circleMask
+      .append("circle")
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .attr("r", smallRadius)
+      .attr("fill", "white");
+
+    svg
+      .selectAll("rect.vertical")
+      .data(range(n))
+      .join("rect")
+      .attr("x", (d) => d * 20)
+      .attr("width", 10)
+      .attr("height", height)
+      .attr("class", "vertical")
+      .attr("mask", "url(#mask2)");
+
+    const mask2 = svg.append("mask").attr("id", "mask2");
+    mask2
+      .append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("fill", "white");
+      // appended a circle to stop the layout crashing of the circle mask
+      // appended after the rect only works
+    mask2
+      .append("circle")
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .attr("r", smallRadius)
+      .attr("fill", "black");
+  }, [height, width]);
+
+  return null;
 }
 
 export default App;
